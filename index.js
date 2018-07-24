910e1d43380d011cefe03c2c6c37e340ea236d08
NIKITA AD <nikita8ad@gmail.com>
2018-07-24 12:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, name){
   var i = katzDeliLine.length + 1;
   katzDeliLine.push(name);
-  return "Welcome "+name+". You are number "+i+" in line";
+  return "Welcome, "+name+". You are number "+i+" in line";
 }
 
 function nowServing(katzDeliLine){
   if(0 === katzDeliLine.length){
     return "There is nobody waiting to be served!";
   } else {
-    return "Currently serving "+katzDeliLine.shift();
+    return "Currently serving "+katzDeliLine.shift()+".";
   }
 }
 
 function currentLine(katzDeliLine){
    if(0 === katzDeliLine.length){
-    return "The line is currently empty";
+    return "The line is currently empty.";
   } else {
     var str;
     for(var i=0; i<katzDeliLine.length; i++){
-      str = str + i+"."+katzDeliLine[i];
+      str = str + (i+1)+"."+katzDeliLine[i];
     }
     return "The line is currently "+str;
   }
