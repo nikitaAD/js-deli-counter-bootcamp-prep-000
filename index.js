27c068509ba23cc30a45e9948eab5a41c1c21ce0
NIKITA AD <nikita8ad@gmail.com>
2018-07-24 11:49:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
-  
+  var i = katzDeliLine.length + 1;
+  katzDeliLine.push(name);
+  return "Welcome ${name}. You are number ${i} in line.";
+}
+
+function nowServing(katzDeliLine){
+  if(0 === katzDeliLine.length){
+    return "There is nobody waiting to be served!";
+  } else {
+    return katzDeliLine.shift();
+  }
+}
+
+function currentLine(katzDeliLine){
+   if(0 === katzDeliLine.length){
+    return "There is nobody in the line!";
+  } else {
+    var str;
+    for(var i=0; i<katzDeliLine.length; i++){
+      str = str + "${i}.${katzDeliLine[i]}";
+    }
+    return "The line is currently ${str}";
+  }
 }
